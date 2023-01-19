10d494bd7cf7c31da73d619abba3a42393619459
Rishika Jain <77011730+rjrishika@users.noreply.github.com>
2023-01-19 16:05:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function fun(click){
+    if(click=='cor'){
+        location.href="2nd.html";
+    }
+    else{
+        location.href="5th.html";
+    }
+
+}
---
+++ script1.js
@@ -0,0 +1,9 @@
+function fun(click){
+    if(click=='cor'){
+        location.href="3rd.html";
+    }
+    else{
+        location.href="5th.html";
+    }
+
+}
