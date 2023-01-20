51f5847472e26fa7abb92d230add07cc16a94d3e
Rishika Jain <77011730+rjrishika@users.noreply.github.com>
2023-01-20 12:58:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function fun(click){
-    if(click=='cor'){
+    document.getElementById('cor').style.backgroundColor = "#32cd32";  
+    if(click!='cor'){
+    document.getElementById(click).style.backgroundColor = "red";  
+    }
+    setTimeout(fun1,3000,click);  
+}
+
+function fun1(click){
+    
+    if(click=='cor'){   
         location.href="2nd.html";
     }
     else{
         location.href="5th.html";
     }
-
 }
